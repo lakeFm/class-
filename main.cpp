a8a1661ec0944cacf5853912516cc8f312890425
maks <maksmakuta227@gmail.com>
2021-04-30 22:07:15 +0300

    [2.1] update
    class++ generate headerOnly and header/source code

--- main.cpp
+++ main.cpp
@@ -4,6 +4,10 @@
 #include <vector>
 #include <iostream>
 
+enum funType{
+    GET,SET
+};
+
 class Variable{
 private:
     std::string type;
@@ -20,8 +24,11 @@ public:
 
     std::string getName(){return this->name;}
     std::string getType(){return this->type;}
-    std::string getterFun(){
-        return  "get" + up(name);
+    std::string fun(funType f){
+        if(f == GET)
+            return  "get" + up(name);
+        else
+            return  "set" + up(name);
     }
 
     std::string getter(bool isConst){
@@ -33,12 +40,40 @@ public:
         ss << "\t" <<"}\n";
         return ss.str();
     }
+    std::string getter(const std::string& classname,bool isConst){
+        std::stringstream ss;
+        ss << "\t" << type << " " << classname  << "::get"<< up(name);
+        if(isConst) ss << "() const{\n";
+        else ss << "(){\n";
+        ss << "\t\t" << " return " << name <<";\n";
+        ss << "\t" <<"}\n";
+        return ss.str();
+    }
+    std::string getterH(bool isConst){
+        std::stringstream ss;
+        ss << "\t" << type << " get"<< up(name);
+        if(isConst) ss << "() const;\n";
+        else ss << "();\n";
+        return ss.str();
+    }
 
     std::string setter(){
         std::stringstream ss;
         ss << "\t" << "void set"<< up(name) << "(" << type << " m" << up(name) << "){\n";
         ss << "\t\t" << " this->" << name <<" = m" << up(name) <<  ";\n";
         ss << "\t" <<"}\n";
+        return ss.str();
+    }
+    std::string setter(const std::string& classname){
+        std::stringstream ss;
+        ss << "\t" << "void " << classname <<"::set"<< up(name) << "(" << type << " m" << up(name) << "){\n";
+        ss << "\t\t" << " this->" << name <<" = m" << up(name) <<  ";\n";
+        ss << "\t" <<"}\n";
+        return ss.str();
+    }
+    std::string setterH(){
+        std::stringstream ss;
+        ss << "\t" << "void set"<< up(name) << "(" << type << " m" << up(name) << ");\n";
         return ss.str();
     }
 
@@ -52,17 +87,14 @@ public:
     }
 
     std::string mod(bool full){
-        std::string tmp = name;
-        if(tmp[0] > 'a' && tmp[0] < 'z')
-            tmp[0] -= 32;
         if(full)
-            return type + " m" +tmp;
+            return type + " m" + up(name);
         else
-            return "m" + tmp;
+            return "m" + up(name);
     }
 
 private:
-    std::string up(const std::string& str){
+    static std::string up(const std::string& str){
         std::string tmp = str;
         if(tmp[0] > 'a' && tmp[0] < 'z')
             tmp[0] -= 32;
@@ -74,13 +106,15 @@ class ClassBuilder{
 private:
     std::string className;
     std::vector<Variable> vars;
+    bool headerOnly;
 public:
-    ClassBuilder(const std::string& classData){
+    ClassBuilder(const std::string& classData,bool onlyH){
+        this->headerOnly = onlyH;
         std::string tmp = classData;
         int a = (int)tmp.find_first_of('(');
         this->className = tmp.substr(0,a);
-        tmp = tmp.substr(a+1,tmp.size()-1);
-        tmp += ',';
+        tmp = tmp.substr(a+1,(int)tmp.find_first_of(')'));
+        tmp[tmp.size() -1] = ',';
         decode(tmp);
     }
 
@@ -89,12 +123,12 @@ public:
         ss << "class " << className << "{\n";
         ss << "private: \n";
         for(Variable v : vars){
-            ss << v.getType() << " " << v.getName() << ";\n";
+            ss << "\t" << v.getType() << " " << v.getName() << ";\n";
         }
         ss << "public: \n";
-        ss << constructor(1) << "\n";
-        ss << constructor(2) << "\n";
-        ss << constructor(3) << "\n";
+        ss << constructor(false,1) << "\n";
+        ss << constructor(false,2) << "\n";
+        ss << constructor(false,3) << "\n";
 
         for(Variable v : vars){
             ss << v.getter(true) << "\n" << v.getter(false) << "\n" << v.setter() << "\n";
@@ -103,8 +137,62 @@ public:
         return ss.str();
     }
 
+    std::string header(){
+        std::stringstream ss;
+        ss << comment(className + ".h") << "\n";
+        ss << "class " << className << "{\n";
+        ss << "private: \n";
+        for(Variable v : vars)
+            ss << "\t" << v.getType() << " " << v.getName() << ";\n";
+        ss << "public: \n";
+
+        ss << constructor(true,1);
+        ss << constructor(true,2);
+        ss << constructor(true,3);
+
+        for(Variable v : vars){
+            ss << v.getterH(true) << v.getterH(false) << v.setterH();
+        }
+        ss << "}\n";
+
+        return ss.str();
+    }
+
+    std::string source(){
+        std::stringstream ss;
+        ss << comment(className + ".cpp") << "\n";
+        ss << includeFile(className) << "\n";
+        ss << '\t' << className << "::" <<constructor(false,1);
+        ss << '\t' << className << "::" <<constructor(false,2);
+        ss << '\t' << className << "::" <<constructor(false,3);
+
+        for(Variable v : vars){
+            ss << v.getter(className,true) << v.getter(className, false) << v.setter(className);
+        }
+        ss << "}\n";
+
+        return ss.str();
+    }
+
+
+
+    static std::string includeFile(const std::string& name){
+        return "#include <" + name + ".h>\n";
+    }
+
+    static std::string comment(const std::string& filename){
+        std::stringstream ss;
+        ss << "// ---------------------------" << "\n" ;
+        ss << "// " << filename << "\n" ;
+        ss << "// ---------------------------" << "\n" ;
+        return ss.str();
+    }
+
     void print(){
-        printf("%s\n",classData().c_str());
+        if(headerOnly)
+            printf("%s\n",classData().c_str());
+        else
+            printf("%s\n",(header()+"\n\n\n"+source()).c_str());
     }
 
 private:
@@ -123,38 +211,55 @@ private:
         return Variable(s.substr(0,q),s.substr(q+1,s.size()));
     }
 
-    std::string constructor(int type){
+    std::string constructor(bool h,int type){
         std::stringstream ss;
+        if(!h) {
+            switch (type) {
+                case 1:
+                    ss << className << "(){\n";
+                    for (Variable v : vars) {
+                        ss << "\t\tthis->" << v.getName() << " = " << v.null() << ";\n";
+                    }
+                    ss << "\t}\n";
+                    break;
+                case 2:
+                    ss << className << "(";
+                    for (int a = 0; a < vars.size(); a++) {
+                        if (a == vars.size() - 1)ss << vars[a].mod(true);
+                        else ss << vars[a].mod(true) << ",";
+                    }
+                    ss << "){\n";
+                    for (Variable v : vars) {
+                        ss << "\t\tthis->" << v.getName() << " = " << v.mod(false) << ";\n";
+                    }
+                    ss << "\t}\n";
+                    break;
+                case 3:
+                    ss << className << "(" << className << " &oth){\n";
+                    for (Variable v : vars) {
+                        ss << "\t\tthis->" << v.getName() << " = oth." << v.fun(GET) << "();\n";
+                    }
+                    ss << "\t}\n";
 
-        switch (type) {
-            case 1: //default
-                ss << '\t' << className <<"(){\n";
-                for(Variable v : vars){
-                    ss << "\t\tthis->" << v.getName() << " = " << v.null() <<";\n";
-                }
-                ss << "\t}\n";
-                break;
-            case 2:
-                ss << '\t' << className <<"(";
-                for(int a = 0;a < vars.size();a++){
-                    if(a == vars.size() -1 )ss << vars[a].mod(true);
-                    else ss << vars[a].mod(true) << ",";
-                }
-                ss << "){\n";
-                for(Variable v : vars){
-                    ss << "\t\tthis->" << v.getName() << " = " << v.mod(false) <<";\n";
-                }
-                ss << "\t}\n";
-                break;
-            case 3:
-                ss << '\t' << className <<"("<< className <<" &oth){\n";
-                for(Variable v : vars){
-                    ss << "\t\tthis->" << v.getName() << " = oth." << v.getterFun() <<"();\n";
-                }
-                ss << "\t}\n";
-
-                break;
-            default: break;
+                    break;
+            }
+        }else{
+            switch (type) {
+                case 1:
+                    ss << '\t' << className << "();\n";
+                    break;
+                case 2:
+                    ss << '\t' << className << "(";
+                    for (int a = 0; a < vars.size(); a++) {
+                        if (a == vars.size() - 1)ss << vars[a].mod(true);
+                        else ss << vars[a].mod(true) << ",";
+                    }
+                    ss << ");\n";
+                    break;
+                case 3:
+                    ss << '\t' << className << "(" << className << " &oth);\n";
+                    break;
+            }
         }
         return ss.str();
     }
@@ -162,8 +267,9 @@ private:
 };
 
 int main(){
+    // player(vec3f position,int id,long socket,array<Item> inventory)
     char data[100]; std::cin.getline(data,100);
-    ClassBuilder builder((std::string(data)));
+    ClassBuilder builder((std::string(data)), true);
     builder.print();
     return 0;
 }
